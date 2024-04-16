71f3ffd17908f93fcacebf216920984be4361c4e
ChuNan Liu <bio.chunanliu@gmail.com>
2024-04-16 21:08:02 +0100

    lots of updates

--- assets/install-piper.sh
+++ assets/install-piper.sh
@@ -1,27 +1,20 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Aim: install piper
 
-shellName=$(basename $SHELL)
-
 # init conda
-source ~/.${shellName}rc
-conda init $shellName > /dev/null 2>&1
-
-# create a py27 environment
-conda create -n piper-py27 python=2.7 -y
+source $HOME/.zshrc
+conda init zsh > /dev/null 2>&1
+conda activate piper-py27
 
 # config
-BASE=$(dirname $(realpath $0))
+BASE=$(dirname $(realpath $0))  # => assets
 
 # install pdb2pqr
-conda activate piper-py27
-mkdir -p $BASE/docking-tools
-tar -zxf haddock.tar.gz -C $BASE/docking-tools/
-tar -zxf piper.tar.gz -C $BASE/docking-tools/
-pushd docking-tools/piper/pdb2pqr-1.9.0
-python scons/scons.py install
-popd
-
-# link the piper executable to PATH and make it executable
-ln -s $BASE/docking-tools/piper/piper $CONDA_PREFIX/bin/piper
+pushd $BASE
+mkdir -p ./docking-tools/
+tar -zxf piper.tar.gz -C ./docking-tools
+pushd ./docking-tools/piper/protein_prep/pdb2pqr-1.9.0
+python ./scons/scons.py install
+popd  # => assets
+sudo ln -s $PWD/docking-tools/piper/piper /usr/local/bin/piper
